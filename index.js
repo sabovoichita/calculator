63c09ef5182b8a73c3dd8adcbeb42a24668759ec
Voichita Maria <sabo_voichita@yahoo.com>
2024-07-11 17:33:07 +0200

    version2: calculator with table

--- index.js
+++ index.js
@@ -48,8 +48,116 @@ function createCalculator() {
 </div>  
     `;
 }
+
+function createCalculatorTable() {
+  return `
+    <h1>Calculator Program in JavaScript</h1>
+    <div class="formstyle1">
+      <form name="form1">
+        <input class="textview" name="textview" />
+      </form>
+      <table>
+        <tr>
+          <td>
+            <input
+              type="button"
+              value="C"
+              class="btn"
+              onclick="form1.textview.value = ''"
+            />
+          </td>
+          <td>
+            <input type="button" value="B" class="btn" onclick="backspace()" />
+          </td>
+          <td>
+            <input type="button" value="/" class="btn" onclick="insert('/')" />
+          </td>
+          <td>
+            <input type="button" value="x" class="btn" onclick="insert('*')" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="button" value="7" class="btn" onclick="insert('7')" />
+          </td>
+          <td>
+            <input type="button" value="8" class="btn" onclick="insert('8')" />
+          </td>
+          <td>
+            <input type="button" value="9" class="btn" onclick="insert('9')" />
+          </td>
+          <td>
+            <input type="button" value="-" class="btn" onclick="insert('-')" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="button" value="4" class="btn" onclick="insert('4')" />
+          </td>
+          <td>
+            <input type="button" value="5" class="btn" onclick="insert('5')" />
+          </td>
+          <td>
+            <input type="button" value="6" class="btn" onclick="insert('6')" />
+          </td>
+          <td>
+            <input type="button" value="+" class="btn" onclick="insert('+')" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="button" value="1" class="btn" onclick="insert('1')" />
+          </td>
+          <td>
+            <input type="button" value="2" class="btn" onclick="insert('2')" />
+          </td>
+          <td>
+            <input type="button" value="3" class="btn" onclick="insert('3')" />
+          </td>
+          <td rowspan="5">
+            <input
+              type="button"
+              value="="
+              class="btn"
+              style="height: 110px"
+              onclick="equal()"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <input
+              type="button"
+              value="0"
+              class="btn"
+              style="width: 106px"
+              onclick="insert('0')"
+            />
+          </td>
+          <td>
+            <input type="button" value="." class="btn" onclick="insert('.')" />
+          </td>
+        </tr>
+      </table>
+    </div>
+    `;
+}
+function insert(num) {
+  document.form1.textview.value = document.form1.textview.value + num;
+}
+function backspace() {
+  var exp = document.form1.textview.value;
+  document.form1.textview.value = exp.substring(0, exp.length - 1);
+}
+function equal() {
+  var exp = document.form1.textview.value;
+  if (exp) {
+    document.form1.textview.value = eval(exp);
+  }
+}
+
 function insertCalculator() {
-  document.body.innerHTML = createCalculator();
+  document.body.innerHTML = createCalculatorTable();
 }
 function initEvents() {
   insertCalculator();
